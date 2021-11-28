a47f10a86328aa32df165bbbe576b8aeaa8673ba
Yonatan <yw692@drexel.edu>
2021-11-28 15:47:20 -0500

    Changed DEBUG value

--- NeuroFlowMoodSelector/settings.py
+++ NeuroFlowMoodSelector/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+8q2n=d!%v)3qa#s=4=&@#7s%^fc+yc4qnyb9z^hl$y990667@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
